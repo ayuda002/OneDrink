f344b17eeb16b0d091b5d9d4a44ff2f683935a93
ayuda002 <yudai02@puc.pu-toyama.ac.jp>
2023-01-09 14:41:38 +0900

    gin_add
    ジンの詳細を追加しました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,31 +47,32 @@ export default function Home() {
         <div className='grid grid-cols-1 lg:grid-cols-4 m-5'>
         <div className='flex flex-col mx-3'>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Beel_data} type={"ビールカクテル"}/>
+        <Accordion_Menu alcohol={Beel_data} type={"ビールカクテル"} url={"/"}/>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Whisky} type={"ハイボール"}/>
+        <Accordion_Menu alcohol={Whisky} type={"ハイボール"} url={"/"}/>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Japan_Wine} type={"日本酒"}/>
+        <Accordion_Menu alcohol={Japan_Wine} type={"日本酒"} url={"/"}/>
         </div>
         <div className='flex flex-col mx-3'>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={shochu} type={"焼酎"}/>
+        <Accordion_Menu alcohol={shochu} type={"焼酎"} url={"/"}/>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Wine} type={"ワイン"}/>
+        <Accordion_Menu alcohol={Wine} type={"ワイン"} url={"/"}/>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Gin} type={"ジンカクテル"}/>
+        <Accordion_Menu alcohol={Gin} type={"ジンカクテル"} url={"/jin_description"}/>
         </div>
         <div className='flex flex-col mx-3'>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Vodka} type={"ウォッカカクテル"}/>
+        <Accordion_Menu alcohol={Vodka} type={"ウォッカカクテル"} url={"/"}/>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Ram} type={"ラムカクテル"}/>
+        <Accordion_Menu alcohol={Ram} type={"ラムカクテル"} url={"/"}/>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Liqueur} type={"リキュールカクテル"}/>
+        <Accordion_Menu alcohol={Liqueur} type={"リキュールカクテル"} url={"/"}/>
         </div>
         <div className='flex flex-col mx-3'>
         <div className='mt-3' />
-        <Accordion_Menu alcohol={Rare} type={"珍しいカクテル"}/>
+        <Accordion_Menu alcohol={Rare} type={"珍しいカクテル"} url={"/"}/>
+        <iframe className='mt-5 m-auto' src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=12&l=ur1&category=amazonstudent&banner=1EXM3ZCBAGEJJSPD8ZG2&f=ifr&linkID=ba00ebab63cbe6aa0d56d7a4b4021c4e&t=onedrink-22&tracking_id=onedrink-22" width="300" height="250" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>
         </div>
         </div>
     </div>
---
+++ pages/jin_description.tsx
@@ -0,0 +1,80 @@
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    CardFooter,
+    Typography,
+  } from "@material-tailwind/react";
+  import { Button } from "@material-tailwind/react";
+import Jin from '../src/detail/jin_des.json';
+import Head from 'next/head'
+import Header from '../components/molecules/header'
+
+type description ={
+  name:string,
+  image_url:string,
+  image_url2:string,
+  image_url3:string,
+  description:string,
+  price:string,
+  url:string;
+}
+
+type Jin = description;
+
+
+export default function Jin_Des(){
+    return(
+     <div className="m-5">
+      <Head>
+        <title>お酒一覧表：ジン</title>
+        <meta name="description" content="お酒初心者の方向けに各お酒の度数と原材料を一覧で表示しました。一部のお酒には豆知識が書いてあります。" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <div className='mx-5 my-5'>
+    <Header />
+    </div>
+    <Card className="w-100">
+      <CardBody className="text-center">
+        <Typography variant="h5" className="mb-2">
+        ジンとは?
+        </Typography>
+        <Typography className="text-left">
+        大麦、ライ麦、ジャガイモなどを原料とした蒸留酒。蒸留酒の中では比較的、個性が強くない上、西ヨーロッパでは古くから知られているため、そのまま飲むだけでなく、カクテルの材料として最も多く使われているものの一つである。
+        </Typography>
+      </CardBody>
+      <CardFooter divider className="flex items-center justify-between py-3">
+        <Typography variant="small" color="gray" className="flex gap-1">
+         参照：<a href="https://ja.wikipedia.org/wiki/%E3%82%B8%E3%83%B3_(%E8%92%B8%E7%95%99%E9%85%92)">ウィキペディア</a>
+        </Typography>
+      </CardFooter>
+    </Card>
+    <div className='grid grid-cols-1 lg:grid-cols-4 my-10 my-5'>
+    {Jin.map((item)=>(
+       <Card className="mx-2">
+       <div className="mx-auto relative">
+       <a className="m-auto" href={item.image_url} target="_blank">
+         <img src={item.image_url2} /></a>
+         <img src={item.image_url3} width="1" height="1" alt=""/>
+       </div>
+       <div className="text-center mt-4">
+       <Typography variant="h5" className="mb-2">
+          <a href={item.url}>{item.name}</a>
+         </Typography>
+       </div>
+       <CardBody className="text-center mb-auto">
+         <Typography className="text-left ">
+         {item.description}
+         </Typography>
+       </CardBody>
+       <CardFooter divider className="flex items-center justify-between py-3">
+         <Typography variant="small">{item.price}</Typography>
+         <a href={item.url}><Button>購入する</Button></a>
+       </CardFooter>
+     </Card>
+    ))}
+    </div>
+    </div>
+    )
+}
