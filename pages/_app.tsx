43a4589632c27922debe6387e213892353b9e842
ayuda002 <yudai02@puc.pu-toyama.ac.jp>
2023-01-17 15:26:35 +0900

    自動デプロイ設定変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
-
+import { GoogleAnalytics } from '../src/lib/gtag'
+import { usePageView } from '../src/lib/gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url:any) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-  return <Component {...pageProps} />
+  usePageView()
+  return(
+    <>
+    <GoogleAnalytics/>
+     <Component {...pageProps} />
+    </>
+  ) 
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "../lib/gtag";
 import Script from "next/script";
 
 export default function Document() {
@@ -23,25 +22,6 @@ export default function Document() {
           "749830446321faa9fac972fc15b942d7"; amzn_assoc_placement = "";
           amzn_assoc_marketplace = "amazon"; amzn_assoc_region = "JP";
         </script> */}
-        <Script
-          id="my-script"
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <Script
-        id="my-script"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-        <Script id="my-script" src="//ws-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&MarketPlace=JP"></Script>
       </Head>
       <body>
         <Main />
