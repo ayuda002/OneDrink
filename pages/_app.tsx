274c1d97275f1285fd79b4e6be6d6746d293e95c
ayuda002 <yudai02@puc.pu-toyama.ac.jp>
2023-04-29 16:29:22 +0900

    Google設定変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { GoogleAnalytics } from '../src/lib/gtag'
-import { usePageView } from '../src/lib/gtag'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { GA_TRACKING_ID, pageview } from "../src/lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  usePageView()
-  return(
+  const router = useRouter();
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、return
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  return (
     <>
-    <GoogleAnalytics/>
-     <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
-  ) 
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,31 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../src/lib/gtag";
 import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <Script
-      id="my-script"
-          dangerouslySetInnerHTML={{
-            __html: `
-            amzn_assoc_ad_type = "link_enhancement_widget"; amzn_assoc_tracking_id
-            = "onedrink-22"; amzn_assoc_linkid =
-            "749830446321faa9fac972fc15b942d7"; amzn_assoc_placement = "";
-            amzn_assoc_marketplace = "amazon"; amzn_assoc_region = "JP";
-          `,
-          }}
-        />
-        {/* <script type="text/javascript">
-          amzn_assoc_ad_type = "link_enhancement_widget"; amzn_assoc_tracking_id
-          = "onedrink-22"; amzn_assoc_linkid =
-          "749830446321faa9fac972fc15b942d7"; amzn_assoc_placement = "";
-          amzn_assoc_marketplace = "amazon"; amzn_assoc_region = "JP";
-        </script> */}
+        {GA_TRACKING_ID && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <body>
         <Main />
